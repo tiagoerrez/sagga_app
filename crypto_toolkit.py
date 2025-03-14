ea6ee19026673665fb60086b5e5d597e9525e815
Santiago Gutierrez <tiago.errez@gmail.com>
2025-03-14 22:58:48 +0100

    fixed UI and added new plots, reduced the number of tabs too

--- crypto_toolkit.py
+++ crypto_toolkit.py
@@ -425,8 +425,8 @@ def cvar_gaussian(r, level=5, modified=False, days=1, window=None):
         # Calculate CVaR
         # For Gaussian distribution, CVaR can be derived as follows:
         # cvar = -(r.mean() + (stats.norm.pdf(z) / (1 - level / 100)) * r.std(ddof=0))
-        cvar = (r.mean() + r.std(ddof=0) * stats.norm.pdf(z) / (level / 100))
-        return cvar * np.sqrt(days)
+        cvar = -(r.mean() + r.std(ddof=0) * np.sqrt(days) * stats.norm.pdf(z) / ( (1-level) / 100))
+        return cvar
     
     else:
         return r.rolling(window).apply(lambda x: rolling_cvar(x, level, days, modified), raw=False).dropna()
